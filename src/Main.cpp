3b165fe260aa008fd30d6e6005e95f59f369e7a6
Traitortots <theholographicgamer@gmail.com>
2022-01-09 02:02:48 -0700

    Initial Setup

---
+++ src/Main.cpp
@@ -0,0 +1,20 @@
+#define WIN
+#ifdef WIN // Windows compilation path
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#else // linux compilation path
+
+// TODO: Add Linux Compilation Path
+
+#endif
+
+#include <stdio.h>
+
+int main()
+{
+    
+
+    return(0);
+}
