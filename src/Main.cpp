8222fecf58a4dde889968ee23b6d6cbf67af04e1
Traitortots <theholographicgamer@gmail.com>
2022-01-10 05:18:29 -0700

    Setting up the windows Socket Connection

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,8 +12,69 @@
 
 #include <stdio.h>
 
+#define DEFAULT_PORT "80"
+
 int main()
 {
+    WSADATA wsaData;
+
+    int iResult;
+
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if(iResult != 0)
+    {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+
+    struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if(iResult != 0)
+    {
+        printf("getaddrinfo failed: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+
+    ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(ListenSocket == INVALID_SOCKET)
+    {
+        printf("Generating Socket failed: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if(iResult == SOCKET_ERROR)
+    {
+        printf("bind failed: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    freeaddrinfo(result);
+
+    iResult = listen(ListenSocket, SOMAXCONN);
+    if(iResult == SOCKET_ERROR)
+    {
+        printf("listen failed: %d\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
     
 
     return(0);
